a12b21b0ec3658e1df52b183a9ce3d1c7afd5794
xssgjnt <1301608557@qq.com>
2022-10-16 22:38:13 +0800

    code144

---
+++ easy/code144.cpp
@@ -0,0 +1,35 @@
+//
+// Created by 13016 on 2022/10/16.
+//
+#include "iostream"
+#include "vector"
+using namespace std;
+
+struct TreeNode {
+         int val;
+         TreeNode *left;
+         TreeNode *right;
+         TreeNode() : val(0), left(nullptr), right(nullptr) {}
+         TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+         TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+
+    void preorder(TreeNode * root,vector<int> & nums){
+        if (!root) return;
+        nums.push_back(root->val);
+        preorder(root->left,nums);
+        preorder(root->right,nums);
+    }
+
+
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> vi;
+        preorder(root,vi);
+        return vi;
+
+
+    }
+};
